f90a3a78b45fc6c6b8d7ad0fabc50fa55f2b52d7
IplinskyLeo <leoipy16@gmail.com>
2022-08-15 14:01:37 -0300

    Version 1.0

--- src/app.js
+++ src/app.js
@@ -1,60 +1,23 @@
-// import { openDb } from "./configDB.js";
-// import {
-//   createTable,
-//   insertPessoa,
-//   updatePessoa,
-//   selectPessoa,
-//   selectPessoas,
-//   deletePessoa,
-// } from "./controller/Pessoa.js";
-
 import express from "express";
+import fs from "fs";
+import https from "https";
+import cors from "cors";
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 import router from "./routes.js";
 app.use(router);
 
-// createTable();
-
-// app.get("/", (req, res) => {
-//   res.send("Hello WorldY");
-// });
-
-// app.get("/pessoas", async (req, res) => {
-//   let pessoas = await selectPessoas();
-//   res.json(pessoas);
-// });
-
-// app.get("/pessoa", async (req, res) => {
-//   let pessoa = await selectPessoa(req.body.id);
-//   res.json(pessoa);
-// });
-
-// app.post("/pessoa", (req, res) => {
-//   insertPessoa(req.body);
-//   res.json({
-//     statusCode: 200,
-//   });
-// });
-
-// app.put("/pessoa", (req, res) => {
-//   if (req.body && !req.body.id) {
-//     res.json({
-//       statusCode: "400",
-//       message: "ID é obrigatório",
-//     });
-//   } else {
-//     updatePessoa(req.body);
-//     res.json({
-//       statusCode: 200,
-//     });
-//   }
-// });
-
-// app.delete("/pessoa", async (req, res) => {
-//   let pessoa = await deletePessoa(req.body.id);
-//   res.json(pessoa);
-// });
-
 app.listen(3000, () => console.log("API rodando na porta 3000"));
+
+https
+  .createServer(
+    {
+      cert: fs.readFileSync("./src/ssl/code.crt"),
+      key: fs.readFileSync("./src/ssl/code.key"),
+    },
+    app
+  )
+  .listen(3001, () => console.log("API rodando em https"));
